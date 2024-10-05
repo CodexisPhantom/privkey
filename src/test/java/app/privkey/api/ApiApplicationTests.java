c4215ee784ebbb1cb8dbdd3d5930b997e9e620fe
CodexisPhantom <gabdostuni47400@gmail.com>
2024-10-05 11:52:13 +0200

    initialization

---
+++ src/test/java/app/privkey/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.privkey.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
